f5b7569636642c12ff53bcf403cf9c2398f3f1c9
Tania Lapalelo <71072931+niatania102@users.noreply.github.com>
2025-01-24 06:27:20 +0700

    feat(root): modify style for project navigation

--- components/ProjectNavigation.tsx
+++ components/ProjectNavigation.tsx
@@ -26,11 +26,11 @@ const ProjectNavigation = ({ projects }: ProjectNavigationProps) => {
     <Grid gutter={{ base: 5, xs: "md", md: "xl", xl: 50 }}>
       {Object.entries(groupedProjects).map(([type, projects]) => (
         <Grid.Col span={{ base: 12, md: 6 }} key={type}>
-          <Title my={"sm"} size={"sm"} c={"#444444"}>{type}</Title>
+          <Title mx={"40px"} my={"sm"} size={"sm"} c={"#444444"}>{type}</Title>
 
             <ul>
             {projects.map((project) => (
-              <li key={project.id}>
+              <li key={project.id} style={{listStyleType: "none"}}>
                 <Link href={`/proyek/${project.slug}`} >{project.name}</Link>
                 </li>
             ))}
